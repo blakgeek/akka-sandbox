1c7b28702fa93ad9c548e568716b875799c1d598
Carlos Lawton <carlos.lawton@altisource.com>
2014-03-20 12:36:45 -0400

    Initial commit

---
+++ src/main/java/com/blakgeek/akka/Arena.java
@@ -0,0 +1,21 @@
+package com.blakgeek.akka;
+
+import akka.actor.ActorSystem;
+
+/**
+ * User: Carlos Lawton
+ * Date: 3/19/14
+ * Time: 8:57 PM
+ */
+public class Arena {
+
+    public static void main(String[] args) {
+        ActorSystem system;
+        if (args.length > 0) {
+            system = ActorSystem.create(args[0]);
+        }
+
+//        ActorRef a = system.actorOf(Props.create(HelloWorld.class), "helloWorld");
+//        system.actorOf(Props.create(Terminator.class, a), "terminator");
+    }
+}
---
+++ src/main/java/com/blakgeek/akka/Contender.java
@@ -0,0 +1,9 @@
+package com.blakgeek.akka;
+
+/**
+ * User: Carlos Lawton
+ * Date: 3/20/14
+ * Time: 11:17 AM
+ */
+public class Contender  {
+}
